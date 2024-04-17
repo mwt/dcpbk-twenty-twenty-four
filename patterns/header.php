0366a7138dfa3fc44b07680190bcac2cd3551dce
Matthew W. Thomas <matthew@mwt.me>
2024-04-17 15:09:19 -0400

    make nav buttons smaller on mobile

--- patterns/header.php
+++ patterns/header.php
@@ -18,12 +18,12 @@
 <div id="greater-nav-menu" class="wp-block-group"><!-- wp:navigation {"icon":"menu","layout":{"type":"flex","justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"margin":{"top":"0"},"blockGap":"var:preset|spacing|20"},"layout":{"selfStretch":"fixed","flexSize":null}}} /-->
 
 <!-- wp:buttons {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"right"},"style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
-<div class="wp-block-buttons" id="nav-buttons"><!-- wp:button {"className":"is-style-outline","fontSize":"medium"} -->
-<div class="wp-block-button has-custom-font-size is-style-outline has-medium-font-size" id="member-area-button"><a class="wp-block-button__link wp-element-button" href="https://www.dcpbk.org/membership/members/"><?php echo __('Member Area', 'twentytwentyfour');?></a></div>
+<div class="wp-block-buttons" id="nav-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline" id="member-area-button"><a class="wp-block-button__link wp-element-button" href="https://www.dcpbk.org/membership/members/"><?php echo __('Member Area', 'twentytwentyfour');?></a></div>
 <!-- /wp:button -->
 
-<!-- wp:button {"className":"is-style-outline","fontSize":"medium"} -->
-<div class="wp-block-button has-custom-font-size is-style-outline has-medium-font-size" id="register-button"><a class="wp-block-button__link wp-element-button" href="https://www.dcpbk.org/membership/join/"><?php echo __('Register', 'twentytwentyfour');?></a></div>
+<!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline" id="register-button"><a class="wp-block-button__link wp-element-button" href="https://www.dcpbk.org/membership/join/"><?php echo __('Register', 'twentytwentyfour');?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
