34a68b11476dc6be1f18c2c5f6c7ebb1ddf9033c
Matthew Thomas <mthomas@ftc.gov>
2024-07-16 07:52:41 -0400

    add wpcode snippets
    - disable comments
    - allow svg uploads
    - disable xmlrpc

--- functions.php
+++ functions.php
@@ -38,12 +38,14 @@ if (!function_exists('dcpbktwentytwentyfour_return_custom_field')) {
     add_shortcode('metalookup', 'dcpbktwentytwentyfour_return_custom_field');
 }
 
-// Handle the blocks named "Forced Shortcode" (use this attribute inside query loops!)
-// Usage example:
-// <!-- wp:shortcode {"metadata":{"name":"Forced Shortcode"}} -->
-// <div>[metalookup field="position" default="Board Member"]</div>
-// <!-- /wp:shortcode -->
-// Source: https://gist.github.com/frzsombor/c53446050ee0bb5017e29b9afb039309
+/**
+ * Handle the blocks named "Forced Shortcode" (use this attribute inside query loops!)
+ * Usage example:
+ * <!-- wp:shortcode {"metadata":{"name":"Forced Shortcode"}} -->
+ * <div>[metalookup field="position" default="Board Member"]</div>
+ * <!-- /wp:shortcode -->
+ * Source: https://gist.github.com/frzsombor/c53446050ee0bb5017e29b9afb039309
+ */
 add_filter('render_block', function ($block_content, $block, $instance) {
     if (isset($block['attrs']) && isset($block['attrs']['metadata']) && isset($block['attrs']['metadata']['name']) && $block['attrs']['metadata']['name'] === 'Forced Shortcode') {
         return do_shortcode($block_content);
@@ -67,3 +69,110 @@ add_filter('wpsc_htaccess_mod_expires', function ($expiry_rules) {
     $expiry_rules[] = 'ExpiresByType text/html "access plus 1 minute"';
     return $expiry_rules;
 }, 1);
+
+/****************************************
+ * The below are snippets from WPCode
+ ****************************************/
+/**
+ * Allow SVG uploads for administrator users.
+ *
+ * @param array $upload_mimes Allowed mime types.
+ *
+ * @return mixed
+ */
+add_filter(
+    'upload_mimes',
+    function ($upload_mimes) {
+        // By default, only administrator users are allowed to add SVGs.
+        // To enable more user types edit or comment the lines below but beware of
+        // the security risks if you allow any user to upload SVG files.
+        if (!current_user_can('administrator')) {
+            return $upload_mimes;
+        }
+
+        $upload_mimes['svg']  = 'image/svg+xml';
+        $upload_mimes['svgz'] = 'image/svg+xml';
+
+        return $upload_mimes;
+    }
+);
+
+/**
+ * Add SVG files mime check.
+ *
+ * @param array        $wp_check_filetype_and_ext Values for the extension, mime type, and corrected filename.
+ * @param string       $file Full path to the file.
+ * @param string       $filename The name of the file (may differ from $file due to $file being in a tmp directory).
+ * @param string[]     $mimes Array of mime types keyed by their file extension regex.
+ * @param string|false $real_mime The actual mime type or false if the type cannot be determined.
+ */
+add_filter(
+    'wp_check_filetype_and_ext',
+    function ($wp_check_filetype_and_ext, $file, $filename, $mimes, $real_mime) {
+
+        if (!$wp_check_filetype_and_ext['type']) {
+
+            $check_filetype  = wp_check_filetype($filename, $mimes);
+            $ext             = $check_filetype['ext'];
+            $type            = $check_filetype['type'];
+            $proper_filename = $filename;
+
+            if ($type && 0 === strpos($type, 'image/') && 'svg' !== $ext) {
+                $ext  = false;
+                $type = false;
+            }
+
+            $wp_check_filetype_and_ext = compact('ext', 'type', 'proper_filename');
+        }
+
+        return $wp_check_filetype_and_ext;
+    },
+    10,
+    5
+);
+
+/**
+ * Completely disable comments in WordPress
+ */
+add_action('admin_init', function () {
+    // Redirect any user trying to access comments page
+    global $pagenow;
+
+    if ($pagenow === 'edit-comments.php') {
+        wp_safe_redirect(admin_url());
+        exit;
+    }
+
+    // Remove comments metabox from dashboard
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+    // Disable support for comments and trackbacks in post types
+    foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+});
+
+// Close comments on the front-end
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+
+// Hide existing comments
+add_filter('comments_array', '__return_empty_array', 10, 2);
+
+// Remove comments page in menu
+add_action('admin_menu', function () {
+    remove_menu_page('edit-comments.php');
+});
+
+// Remove comments links from admin bar
+add_action('admin_bar_menu', function () {
+    remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+}, 0);
+
+/**
+ * Disable XML-RPC 
+ */
+add_filter('xmlrpc_enabled', '__return_false');
