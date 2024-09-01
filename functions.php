7ed7dbd231822135c802877ca0ef490899885c70
Matthew W. Thomas <matthew@mwt.me>
2024-08-31 21:54:31 -0400

    Add SMTP mail to functions.php

--- functions.php
+++ functions.php
@@ -75,6 +75,30 @@ add_filter('quicklink_options', function ($options) {
     return $options;
 });
 
+/**
+ * This function will connect wp_mail to your authenticated
+ * SMTP server. This improves reliability of wp_mail, and 
+ * avoids many potential problems.
+ *
+ * For instructions on the use of this script, see:
+ * https://butlerblog.com/easy-smtp-email-wordpress-wp_mail/
+ * 
+ * Values for constants are set in wp-config.php
+ */
+add_action('phpmailer_init', 'send_smtp_email');
+function send_smtp_email($phpmailer)
+{
+    $phpmailer->isSMTP();
+    $phpmailer->Host       = SMTP_HOST;
+    $phpmailer->SMTPAuth   = SMTP_AUTH;
+    $phpmailer->Port       = SMTP_PORT;
+    $phpmailer->Username   = SMTP_USER;
+    $phpmailer->Password   = SMTP_PASS;
+    $phpmailer->SMTPSecure = SMTP_SECURE;
+    $phpmailer->From       = SMTP_FROM;
+    $phpmailer->FromName   = SMTP_NAME;
+}
+
 /*
 // WP Total Cache: Set Cache-Control and Expires headers for HTML files
 // Set Cache-Control and Expires to 1 hour
